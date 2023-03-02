ace04eaa89d6fdf18a063650670e6ab9bb14c988
Henry Percy <henryppercy@gmail.com>
2023-03-02 14:59:09 +0000

    Craeted modal element with animations

---
+++ src/components/Modal.vue
@@ -0,0 +1,50 @@
+<script setup>
+defineProps({
+  show: Boolean,
+});
+</script>
+
+<template>
+  <Transition
+      enter-from-class="opacity-0 scale-125"
+      enter-to-class="opacity-100 scale-100"
+      enter-active-class="transition duration-300"
+      leave-active-class="transition duration-300"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
+  >
+    <div class="modalMask" v-if="show">
+      <div class="modalContainer">
+        <div>
+          <slot>Default body</slot>
+        </div>
+
+        <footer class="mt-3 pt-2 border-t text-sm">
+          <slot name="footer">
+            <button @click="$emit('close')" class="bg-gray-300 rounded-3xl py-2 px-3 hover:bg-gray-400">Default Close</button>
+          </slot>
+        </footer>
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<style scoped>
+.modalMask {
+  margin: 0;
+  position: fixed;
+  inset: 0;
+  background: rgba(0,0,0,.6);
+  display: grid;
+  place-items: center;
+}
+
+.modalContainer {
+  background: white;
+  padding: 2rem;
+  width: 80vw;
+  max-width: 500px;
+  border-radius: 7px;
+}
+
+</style>
---
+++ src/components/Teams/AddMemberModal.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Modal from "@/components/Modal.vue";
+import {ref} from "vue";
+import {useTeamStore} from "@/stores/TeamStore";
+
+let showModal = ref(false);
+let team = useTeamStore();
+
+
+</script>
+
+<template>
+  <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded disabled:bg-gray-400"
+          :disabled="! team.spotsRemaining"
+          @click="showModal = true"
+  >Add Member ({{ team.spotsRemaining }} Spots Left)
+  </button>
+
+  <Teleport to="body">
+    <Modal :show="showModal" @close="showModal = false" class="mt-6">
+      <p>Need to add a new member to your team?</p>
+
+      <form>
+        <div class="flex gap">
+          <input type="email" placeholder="email" class="flex-1">
+          <button class="text-sm bg-gray-300 rounded-lg py-1 px-3 hover:bg-gray-400">Add</button>
+        </div>
+      </form>
+    </Modal>
+  </Teleport>
+</template>
